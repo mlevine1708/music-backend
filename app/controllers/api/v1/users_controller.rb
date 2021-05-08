190e2efcfdc8beca58c2c16eccbfbbc5a89a52dd
Micah Levine <micah.levine@outlook.com>
2021-05-08 17:53:34 -0400

    added user methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json: @users 
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render json: {error: 'Error creating user'}
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+        
+    end
 end
